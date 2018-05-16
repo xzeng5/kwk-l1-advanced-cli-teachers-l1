dfcc5f4bd860af612c44be0a99d28ca592de3dde
sammarcus <sam.marcus@me.com>
2018-05-16 19:25:52 -0400

    test edits

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
-describe 'directories' do
-
-    it 'has the first directory created' do
+describe 'directory structure' do
+  it 'has the first directory created' do
     expect(File).to be_directory ('./this_is_a_directory_made_in_a_script')
   end
 
@@ -11,12 +10,12 @@ describe 'directories' do
   end
 end
 
-describe 'files are created' do
+describe 'file structure' do
   it 'first file is created' do
     expect(File).to exist('./this_is_a_directory_made_in_a_script/first_file.txt'), "Did you create a file named first_file.txt?"
   end
 
-  it 'second file exists' do
+  it 'second file is created' do
     expect(File).to exist('./another_directory/second_file.txt'), "Did you create a file named ./another_directory/second_file.txt?"
   end
 end
