3d1a6fd3a305aa40e642ab2e2a002b96c87401f7
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-11 23:00:48 +0000

    automatically backed up by learn

---
+++ development/test_script.sh
@@ -0,0 +1,6 @@
+ls;
+mkdir this_is_a_directory_made_in_a_script;
+mkdir another_directory;
+touch this_is_a_directory_made_in_a_script/first_file.txt;
+touch this_is_a_directory_made_in_a_script/second_file.txt;
+mv this_is_a_directory_made_in_a_script/second_file.txt another_directory
